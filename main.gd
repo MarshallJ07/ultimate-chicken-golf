22f54e46b6d893459a81a7868e7b9a15bc58bb7e
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-02 22:24:03 -0400

    nametag

--- main.gd
+++ main.gd
@@ -16,10 +16,12 @@ func on_host_created() -> void:
 func spawn_player(peer_id:int) -> void:
 	var new_player := PLAYER_CONTROLLER.instantiate() as CharacterBody3D
 	new_player.name = str(peer_id)
+	
 	add_child(new_player)
 	initialize_player(new_player)
 	getName.rpc_id(peer_id,peer_id)
 	spawn_ball(peer_id)
+	new_player.get_node("nametag").text = playerNames[peer_id]
 	
 @rpc("any_peer","call_local","reliable")
 func getName(peer_id:int) -> void:
