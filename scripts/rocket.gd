47402c25536135be4d80aa4eb3e282531939b6d3
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-07 16:17:49 -0400

    rocket stuff

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -8,3 +8,25 @@ func _ready() -> void:
 	$MultiplayerSynchronizer.set_multiplayer_authority(1)
 	if not is_multiplayer_authority():
 		get_child(0).freeze = true
+
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if not is_multiplayer_authority():
+		return
+	if body.name != get_parent().get_parent().get_node(str(id)).name and body.name != $rocketCollider.name:
+		explode_everywhere.rpc(id)
+	
+	
+#EVERYONE
+@rpc("any_peer", "call_local", "reliable")
+func explode_everywhere(peer_id) -> void:
+	if id == peer_id:
+		$explosion.position = $rocketCollider.position
+		$explosion.restart()
+		$rocketCollider.queue_free()
+
+
+
+func _on_explosion_finished() -> void:
+	queue_free()
