d1700ad0bdb3b3f2afe9a812d1434565c0f553ad
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-07 01:38:15 -0400

    multiplayer update

--- scripts/rigid_body_player.gd
+++ scripts/rigid_body_player.gd
@@ -8,9 +8,21 @@ func _ready() -> void:
 	$Timer.start()
 
 func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
+	follow_ragdoll_everywhere.rpc()
+	
+@rpc("any_peer","call_local","reliable")
+func follow_ragdoll_everywhere() -> void:
 	get_parent().get_node(str(multiplayer.get_unique_id())).position = position
 
 func _on_timer_timeout() -> void:
+	if not is_multiplayer_authority():
+		return
+	reset_character_everywhere.rpc()
+	
+@rpc("any_peer","call_local","reliable")
+func reset_character_everywhere() -> void:
 	get_parent().get_node(str(multiplayer.get_unique_id())).position = position
 	get_parent().get_node(str(multiplayer.get_unique_id())).externalVelocity = linear_velocity
 	get_parent().get_node(str(multiplayer.get_unique_id())).get_node("Collider").disabled = false
