223c4a071efe80e260b059087034d4f7ce1ca663
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-07 14:25:51 -0400

    Update rocket.gd

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -18,6 +18,7 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 func get_collisions() -> void:
 	for hit in $rocketCollider/explosion/explosion.get_overlapping_bodies():
 		if hit is CharacterBody3D:
+			print(hit.name)
 			create_ragdoll_everywhere.rpc(hit.name)
 			
 			
