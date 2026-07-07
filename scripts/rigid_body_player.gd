847d6e16044143847aa91888c0afb8c35fb3a5ad
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-07 02:59:21 -0400

    f

--- scripts/rigid_body_player.gd
+++ scripts/rigid_body_player.gd
@@ -10,24 +10,24 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority():
 		return
-	follow_ragdoll_everywhere.rpc()
+	follow_ragdoll_everywhere.rpc(id)
 	
 @rpc("any_peer","call_local","reliable")
-func follow_ragdoll_everywhere() -> void:
+func follow_ragdoll_everywhere(ragdollID) -> void:
 	if not is_multiplayer_authority():
 		return
-	if not id == multiplayer.get_unique_id():
+	if not id == ragdollID:
 		return
 	get_parent().get_node(str(multiplayer.get_unique_id())).position = position
 
 func _on_timer_timeout() -> void:
 	if not is_multiplayer_authority():
 		return
-	reset_character_everywhere.rpc()
+	reset_character_everywhere.rpc(id)
 	
 @rpc("any_peer","call_local","reliable")
-func reset_character_everywhere() -> void:
-	if id == multiplayer.get_unique_id():
+func reset_character_everywhere(ragdollID) -> void:
+	if id == ragdollID:
 			get_parent().get_node(str(multiplayer.get_unique_id())).externalVelocity = linear_velocity
 			get_parent().get_node(str(multiplayer.get_unique_id())).get_node("Collider").disabled = false
 			get_parent().get_node(str(multiplayer.get_unique_id())).get_node("Mesh").show()
