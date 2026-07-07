36e08f88c83c56aed922459a6f313d2b52090d04
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-07 01:46:13 -0400

    f

--- scripts/rigid_body_player.gd
+++ scripts/rigid_body_player.gd
@@ -19,12 +19,16 @@ func follow_ragdoll_everywhere() -> void:
 func _on_timer_timeout() -> void:
 	if not is_multiplayer_authority():
 		return
-	reset_character_everywhere.rpc_id(1)
+	reset_character.rpc_id(1)
+	reset_character_everywhere.rpc(1)
+	
+@rpc("any_peer","call_local","reliable")
+func reset_character() -> void:
+	get_parent().get_node(str(multiplayer.get_unique_id())).position = position
+	get_parent().get_node(str(multiplayer.get_unique_id())).externalVelocity = linear_velocity
 	
 @rpc("any_peer","call_local","reliable")
 func reset_character_everywhere() -> void:
-	get_parent().get_node(str(multiplayer.get_unique_id())).position = position
-	get_parent().get_node(str(multiplayer.get_unique_id())).externalVelocity = linear_velocity
 	get_parent().get_node(str(multiplayer.get_unique_id())).get_node("Collider").disabled = false
 	get_parent().get_node(str(multiplayer.get_unique_id())).get_node("Mesh").show()
 	queue_free()
