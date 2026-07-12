efa4ab0ef4b1cf300149e21e608b920a84cd43d8
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-12 13:01:30 -0400

    Update ball.gd

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -56,7 +56,7 @@ func winnerText() -> void:
 	print(get_parent().get_parent().playerNames)
 	print(name)
 	print('name  ',get_parent().get_parent().playerNames[str(name.to_int())]+" Wins")
-	displayText.rpc(get_parent().get_parent().playerNames[str(name.to_int())]+" Wins")
+	get_parent().get_parent().get_node('1').displayText.rpc(get_parent().get_parent().playerNames[str(name.to_int())]+" Wins")
 	
 	
 	
