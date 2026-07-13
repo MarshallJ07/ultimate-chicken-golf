062e251af829b12f80be76d0ac0ef09466718a21
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-13 19:00:11 -0400

    Update start.gd

--- scripts/start.gd
+++ scripts/start.gd
@@ -7,6 +7,10 @@ func _on_host_pressed() -> void:
 
 
 func _on_pressed() -> void:
+	show_crossair.rpc()
+	queue_free()
+
+@rpc("any_peer","call_local","reliable")
+func show_crossair() -> void:
 	$"../ColorRect".show()
 	$"../ColorRect2".show()
-	queue_free()
